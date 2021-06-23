452a298e9c285e5ce42050aea116718abf62d348
whoname01 <70155241+itsdominus@users.noreply.github.com>
2021-06-23 20:23:13 +0300

    Update Message.py
    Updatescript 1.71

--- android/Message.py
+++ android/Message.py
@@ -5,36 +5,82 @@
 # // telegram — @thisDominus
 # // discord — whoname00#8684
 
-from os import _exit
 import amino
 import time
+import pyfiglet
+import getpass
+from colorama import Fore, Style
+from os import _exit
 
-print("~ ============================== ~")
-print("|        Message Amino Bot       |")
-print("|          by whoname00          |")
-print("|     telegram — @thisDominus    |")
-print("~ ============================== ~")
-print("")
-email = input("Email: ")
-time.sleep(.25)
-password = input("Password: ")
-print("")
-client = amino.Client()
-try:
-  client.login(email=email, password=password)
-except:
-  print("\n\nIncorrect account or email, password.\nPlease re-login or check password and email account.\n\n")
-subclients = client.sub_clients()
-for name, id in zip(subclients.name, subclients.comId):
-	comId = input("Type community id: ")
-	subclient = amino.SubClient(comId=comId, profile=client.profile)
-	for chatName, chatid in zip(subclient.get_chat_threads().title, subclient.get_chat_threads().chatId):
-	  chatId = input("Type chatId: ")
-	  print("MessageType: 1, 50, 58, 51, 100, 110, 115")
-	  msgtype = input("Type message type: ")
-	  while True:
-	    try:
-	      msg = input("Type message: ")
-	      subclient.send_message(message=msg, chatId=chatId, messageType=msgtype)
-	    except:
-	      pass
+print(Fore.GREEN + Style.NORMAL + '''
+Script by whoname00
+telegram - @thisDominus
+''')
+print(pyfiglet.figlet_format("AminoMessage", font="contessa"))
+print('''
+1. Send message
+2. List of working msgType
+3. Exit from script
+''')
+selectnum = str(input("> Choose number: "))
+
+def listmsg():
+    print("""
+    Working msgType:
+    109
+
+    Restart this script -  to back choosing!
+    """)
+    _exit(1)
+
+    # // List msgType.
+
+def exitscript():
+    print("""
+    Done!
+    You're successfully exited from script!
+
+    Restart this script -  to back choosing!
+    """)
+    _exit(1)
+
+    # // Exit function.
+
+# // Selection
+
+if selectnum == "1":
+    # // Login
+
+    email = str(input("> Your email: "))
+    time.sleep(3)
+    password = getpass.getpass("> Your password: ")
+
+    # // Login
+
+    client = amino.Client()
+    try:
+        client.login(email=email, password=password)
+    except:
+        print("Incorrent password, try again!")
+        _exit(1)
+    clients = client.sub_clients(size=100)
+    for x, name in enumerate(clients.name, 1):
+        print(f"{x}.{name}")
+        communityid = clients.comId[int(input("Выберите сообщество/Select the community: "))-1]
+        sub_client = amino.SubClient(comId=communityid, profile=client.profile)
+        chats = sub_client.get_chat_threads(size=150)
+        for z, title in enumerate(chats.title, 1):
+            print(f"{z}.{title}")
+            chatx = chats.chatId[int(input("Выберите чат/Select the chat: "))-1]
+            msgtype = str(input("> Type msgType: "))
+            while True:
+                try:
+                    msg = input("> Type your message: ")
+                    sub_client.send_message(message=msg, chatId=chatx, messageType=msgtype)
+                except:
+                    pass
+elif selectnum == "2":
+    listmsg()
+
+elif selectnum == "3":
+    exitscript()
