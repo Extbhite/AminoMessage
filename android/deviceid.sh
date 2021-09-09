a435457e5b4f605c1f399d7e87d5a9bbaffd71d1
whoname01 <70155241+itsdominus@users.noreply.github.com>
2021-09-09 17:06:14 +0300

    Delete deviceid.sh

--- android/deviceid.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-printf "Paste your working deviceid: "
-read device
-printf "{\"device_id\": \"$device\", \"device_id_sig\": \"AaauX/ZA2gM3ozqk1U5j6ek89SMu\", \"user_agent\": \"Dalvik/2.1.0 (Linux; U; Android 7.1; LG-UK495 Build/MRA58K; com.narvii.amino.master/3.3.33180)\"}">device.json
